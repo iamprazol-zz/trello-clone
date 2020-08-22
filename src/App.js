9c3a0a43c07a838ddc7d939a6c2bef6e9c5d8e81
Prajjwal Poudel <iamprazol@gmail.com>
2020-08-22 16:15:45 +0545

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>This is Trello clone</h1>
+    </div>
+  );
+}
+
+export default App;
